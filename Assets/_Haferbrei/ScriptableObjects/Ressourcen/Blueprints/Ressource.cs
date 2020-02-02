6b8325c6a9d7cd7d3acb2698fe0874bf8e8f556a
Martin_Home <martin.kloeckener@gmail.com>
2020-02-02 18:45:47 +0100

    stuff

---
+++ Assets/_Haferbrei/ScriptableObjects/Ressourcen/Blueprints/Ressource.cs
@@ -0,0 +1,12 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Haferbrei {
+[CreateAssetMenu(fileName = "Ressource", menuName = "Scriptable Objects/Ressourcen/Ressource", order = 0)]
+public class Ressource : ScriptableObject
+{
+    
+}
+}
