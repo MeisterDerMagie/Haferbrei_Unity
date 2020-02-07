9e6b3196149e0df64f68a9918c6d0f805c9fb7f0
Martin_Home <martin.kloeckener@gmail.com>
2020-02-07 11:54:33 +0100

    Camera zoom (wip)

--- Assets/_Haferbrei/Scripts/Camera/Camera_Zoom.cs
+++ Assets/_Haferbrei/Scripts/Camera/Camera_Zoom.cs
@@ -1,11 +1,42 @@
 ﻿//(c) copyright by Martin M. Klöckener
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 
 namespace Haferbrei {
 public class Camera_Zoom : MonoBehaviour
 {
+    [SerializeField, BoxGroup("Settings"), Min(0f)] private float zoomSpeed;
+    [SerializeField, BoxGroup("Settings"), Min(0f)] private float sensitivity;
+    [SerializeField, BoxGroup("Settings"), Min(0f)] private float minZoomValue;
+    [SerializeField, BoxGroup("Settings"), Min(0f)] private float maxZoomValue;
+    [SerializeField, BoxGroup("Settings"), Required] private AnimationCurve zoomSensitivityCurve;
+    [SerializeField, BoxGroup("References"), Required] private Camera mainCamera;
+
+    [SerializeField, BoxGroup("Info"), ReadOnly] private float currentPositionOnCurve;
+    [SerializeField, BoxGroup("Info"), ReadOnly] private float newSize;
     
+    private Tween zoomTween;
+
+    private void OnEnable() => newSize = mainCamera.orthographicSize;
+
+    private void Update()
+    {
+
+        currentPositionOnCurve = Wichtel.MathW.Remap(newSize, minZoomValue, maxZoomValue, 0f, 1f);
+
+        newSize += Input.mouseScrollDelta.y * sensitivity * (zoomSensitivityCurve.Evaluate(currentPositionOnCurve));
+        newSize = Mathf.Clamp(newSize, minZoomValue, maxZoomValue);
+
+        if (newSize != mainCamera.orthographicSize)
+        {
+            zoomTween.Kill();
+            zoomTween = mainCamera.DOOrthoSize(newSize, zoomSpeed).SetEase(Ease.OutExpo);
+        }
+    }
 }
 }
