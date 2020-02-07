b9e4b4f6ff4345ad5af4212a652278f1baf178b2
Martin_Home <martin.kloeckener@gmail.com>
2020-02-07 01:09:45 +0100

    Camera movement

---
+++ Assets/_Haferbrei/Scripts/Camera/Camera_Pan.cs
@@ -0,0 +1,54 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Haferbrei {
+public class Camera_Pan : MonoBehaviour
+{
+    [SerializeField] private Camera gameCamera;
+    [SerializeField, Range(0f, 1f)] private float dampingAmount;
+    [SerializeField, ReadOnly] private Vector3 mousePositionBefore;
+    [SerializeField, ReadOnly] private Vector3 mousePositionDelta;
+    [SerializeField, ReadOnly] private Vector3 dampingDelta;
+    [SerializeField, ReadOnly] private bool dampMovement;
+    
+    
+    
+    private void Update()
+    {
+        mousePositionDelta = mousePositionBefore - gameCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            dampMovement = false;
+        }
+        
+        if (Input.GetMouseButton(1))
+        {
+            transform.position += mousePositionDelta;
+            dampingDelta = mousePositionDelta;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            dampMovement = true;
+        }
+        
+        Damp();
+
+        mousePositionBefore = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void Damp()
+    {
+        if (!dampMovement) return;
+
+        transform.position += dampingDelta;
+
+        dampingDelta *= dampingAmount;
+    }
+}
+}
---
+++ Assets/_Haferbrei/Scripts/Camera/Camera_PanScreenEdge.cs
@@ -0,0 +1,82 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Wichtel.Extensions;
+
+namespace Haferbrei {
+public class Camera_PanScreenEdge : MonoBehaviour
+{
+    [SerializeField] private float edgeThreshold;
+    [SerializeField] private float scrollSpeed;
+    [SerializeField, Range(0f, 1f)] private float dampingAmount;
+    [SerializeField, ReadOnly] private Vector2 mousePosition;
+    [SerializeField, ReadOnly] private Vector3 deltaPos;
+    [SerializeField, ReadOnly] private Vector3 dampingDelta;
+    [SerializeField, ReadOnly] private bool isScrolling;
+    
+    
+    private void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        deltaPos = Vector3.zero;
+
+        isScrolling = false;
+        
+        //left screen edge
+        if (mousePos.x < edgeThreshold)
+        {
+            deltaPos.x -= scrollSpeed;
+            isScrolling = true;
+        }
+        
+        //right screen edge
+        else if (mousePos.x > Screen.width - edgeThreshold)
+        {
+            deltaPos.x += scrollSpeed;
+            isScrolling = true;
+        }
+
+        //lower screen edge
+        if (mousePos.y < edgeThreshold)
+        {
+            deltaPos.y -= scrollSpeed;
+            isScrolling = true;
+        }
+        
+        //upper screen edge
+        else if (mousePos.y > Screen.height - edgeThreshold)
+        {
+            deltaPos.y += scrollSpeed;
+            isScrolling = true;
+        }
+
+        PreventScrollingIfCursorOutsideOfGameWindow(mousePos);
+
+        if(isScrolling) dampingDelta = deltaPos;
+        Damp();
+
+        transform.position += deltaPos;
+    }
+
+    private void Damp()
+    {
+        if (isScrolling) return;
+        dampingDelta *= dampingAmount;
+        transform.position += dampingDelta;
+    }
+
+    private void PreventScrollingIfCursorOutsideOfGameWindow(Vector3 mousePos)
+    {
+        //prevent scrolling if cursor is outisde of the game view
+        if (mousePos.x < -edgeThreshold || mousePos.x > Screen.width + edgeThreshold || mousePos.y < -edgeThreshold || mousePos.y > Screen.height + edgeThreshold)
+        {
+            isScrolling = false;
+            deltaPos = Vector3.zero;
+        } 
+    }
+}
+}
