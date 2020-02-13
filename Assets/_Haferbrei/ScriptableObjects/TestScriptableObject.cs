beafa2dfe4c6be2a92fb3698cac55857d8a7a1d3
Martin_Home <martin.kloeckener@gmail.com>
2020-02-13 13:21:45 +0100

    Test

---
+++ Assets/_Haferbrei/ScriptableObjects/TestScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Haferbrei {
+[CreateAssetMenu(fileName = "asdfasdf", menuName = "asdfdasf", order = 0)]
+public class TestScriptableObject : ScriptableObject
+{
+    public int testInt;
+    public float testFloat;
+}
+}
