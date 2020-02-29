458ce002d19c8927ff8946b12326a133176706b2
Martin_Home <martin.kloeckener@gmail.com>
2020-02-29 01:33:57 +0100

    begonnen, Tooltips zu implementieren

---
+++ Assets/_Haferbrei/Scripts/UI/HasTooltip.cs
@@ -0,0 +1,33 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Haferbrei{
+public class HasTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField, BoxGroup("Settings")] private string title;
+
+    private void ShowTooltip()
+    {
+        Debug.Log("Entered tooltip area!");
+    }
+
+    private void HideTooltip()
+    {
+        Debug.Log("Left tooltip area!");
+    }
+    
+    #region MouseEvents
+
+    public void OnPointerEnter(PointerEventData eventData) => ShowTooltip();
+    public void OnPointerExit(PointerEventData eventData) => HideTooltip();
+    private void OnMouseExit() => HideTooltip();
+    private void OnMouseEnter() => ShowTooltip();
+    
+    #endregion
+}
+}
