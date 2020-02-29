3147bcd3ee804346575dfe9cf86ac1b09ef57b1a
Martin_Home <martin.kloeckener@gmail.com>
2020-02-29 12:02:34 +0100

    Tooltip

--- Assets/_Haferbrei/Scripts/UI/HasTooltip.cs
+++ Assets/_Haferbrei/Scripts/UI/HasTooltip.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MEC;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -10,23 +11,36 @@ namespace Haferbrei{
 public class HasTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField, BoxGroup("Settings")] private string title;
+    [SerializeField, BoxGroup("Settings"), Required] private float delay;
+
+    private CoroutineHandle countdown;
+    
+    private void OnEnterTooltipArea()
+    {
+        countdown = Timing.CallDelayed(delay, ShowTooltip);
+    }
+
+    private void OnLeaveTooltipArea()
+    {
+        Timing.KillCoroutines(countdown);
+    }
 
     private void ShowTooltip()
     {
-        Debug.Log("Entered tooltip area!");
+        Debug.Log("Show tooltip!");
     }
 
     private void HideTooltip()
     {
-        Debug.Log("Left tooltip area!");
+        Debug.Log("Hide tooltip!");
     }
     
     #region MouseEvents
 
-    public void OnPointerEnter(PointerEventData eventData) => ShowTooltip();
-    public void OnPointerExit(PointerEventData eventData) => HideTooltip();
-    private void OnMouseExit() => HideTooltip();
-    private void OnMouseEnter() => ShowTooltip();
+    public void OnPointerEnter(PointerEventData eventData) => OnEnterTooltipArea();
+    public void OnPointerExit(PointerEventData eventData) => OnLeaveTooltipArea();
+    private void OnMouseExit() => OnLeaveTooltipArea();
+    private void OnMouseEnter() => OnEnterTooltipArea();
     
     #endregion
 }
