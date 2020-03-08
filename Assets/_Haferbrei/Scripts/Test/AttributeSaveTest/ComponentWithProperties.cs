d2270529722a4b91eec1a68c92cf247b557c2df4
Martin_Home <martin.kloeckener@gmail.com>
2020-03-08 13:50:16 +0100

    Reflection Test wip

---
+++ Assets/_Haferbrei/Scripts/Test/AttributeSaveTest/ComponentWithProperties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Haferbrei{
+public class ComponentWithProperties : MonoBehaviour
+{
+    [Saveable] public float floatWithAttribute;
+    public float floatWithoutAttribute;
+    [Saveable] public Vector3 vector3Test;
+    [Saveable] public Ressource ressourceTest;
+    
+    [Saveable] public float propertyTest { get; set; }
+}
+
+[AttributeUsage(AttributeTargets.Field|AttributeTargets.Property)]
+public class SaveableAttribute : Attribute
+{
+    
+}
+}
---
+++ Assets/_Haferbrei/Scripts/Test/AttributeSaveTest/ReflectOtherComponent.cs
@@ -0,0 +1,37 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Haferbrei {
+public class ReflectOtherComponent : MonoBehaviour
+{
+    public Component componentToReflect;
+    
+    public List<FieldInfo> fields = new List<FieldInfo>();
+    public List<PropertyInfo> properties = new List<PropertyInfo>();
+    
+    
+    [Button]
+    public void GetFieldValueByReflection()
+    {
+        var componentType = componentToReflect.GetType();
+        Debug.Log("componentType: " + componentType.Name);
+
+        var fieldsbla = componentType.GetFields().Where(f => f.GetCustomAttributes(typeof(SaveableAttribute)).Any()); //get all fields marked with SaveableAttribute
+
+        foreach (var field in fieldsbla)
+        {
+            var fieldValue = field.GetValue(field);
+            Debug.Log(field.Name + ": " + fieldValue.ToString());
+        }
+        
+        //var field = componentType.GetField("floatTest");
+        //var fieldValue = field.GetValue(field);
+        //Debug.Log(field.Name + ": " + fieldValue);
+    }
+}
+}
