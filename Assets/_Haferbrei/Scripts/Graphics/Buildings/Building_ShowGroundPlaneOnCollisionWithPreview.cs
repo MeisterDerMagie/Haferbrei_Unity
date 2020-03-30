dfea8f10ea1903b1ceb7ce6099b34a2cc1d27331
Martin_Home <martin.kloeckener@gmail.com>
2020-03-30 23:42:09 +0200

    Tags

---
+++ Assets/_Haferbrei/Scripts/Graphics/Buildings/Building_ShowGroundPlaneOnCollisionWithPreview.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.Tags;
+using UnityEngine;
+
+public class Building_ShowGroundPlaneOnCollisionWithPreview : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TriggerEnter! HasTag = " + other.gameObject.HasTag("BuildingPreview"));
+        if (!other.gameObject.HasTag("BuildingPreview")) return;
+
+        other.GetComponent<BuildingGraphicSwitch>().GraphicMode = BuildingGraphicSwitch.BuildingGraphicMode.GroundPlane;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.gameObject.HasTag("BuildingPreview")) return;
+
+        other.GetComponent<BuildingGraphicSwitch>().GraphicMode = BuildingGraphicSwitch.BuildingGraphicMode.Building;
+    }
+}
