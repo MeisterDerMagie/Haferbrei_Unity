440239ee167d6602ed41633f7bf157033813f2bc
Martin_Home <martin.kloeckener@gmail.com>
2020-04-23 18:26:59 +0200

    Speichern und Laden View Kleinigkeiten

--- Assets/_Haferbrei/Scripts/SaveAndLoad/DeleteFile.cs
+++ Assets/_Haferbrei/Scripts/SaveAndLoad/DeleteFile.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Doozy.Engine.UI;
 
 namespace Haferbrei{
 public static class DeleteFile
@@ -6,8 +8,16 @@ public static class DeleteFile
     public static void Delete(string _filePath)
     {
         if (!File.Exists(_filePath)) return;
-        
-        File.Delete(_filePath);
+
+        try
+        {
+            File.Delete(_filePath);
+        }
+        catch (Exception e)
+        {
+            UIPopup popup = UIPopup.GetPopup("FehlerBeimLoeschenDesSpielstands");
+            popup.Show();
+        }
     }
 }
 }
