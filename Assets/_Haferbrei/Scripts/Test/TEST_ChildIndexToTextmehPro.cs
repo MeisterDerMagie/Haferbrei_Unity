afe7428caeffa9569a444b95ee5cfba6a9d7039c
Martin_Home <martin.kloeckener@gmail.com>
2020-05-09 15:00:21 +0200

    wichtel

---
+++ Assets/_Haferbrei/Scripts/Test/TEST_ChildIndexToTextmehPro.cs
@@ -0,0 +1,20 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Haferbrei {
+[ExecuteInEditMode]
+public class TEST_ChildIndexToTextmehPro : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    private void Update()
+    {
+        text.text = transform.GetSiblingIndex().ToString();
+        gameObject.name = "GameObject " + transform.GetSiblingIndex();
+    }
+}
+}
